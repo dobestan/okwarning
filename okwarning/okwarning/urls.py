438f828613d705c98f4b4ea2034e987c07497731
Suchan An <dobestan@gmail.com>
2015-12-18 14:58:00 +0900

    init NewWarningTemplateView : render new warning template

--- okwarning/okwarning/urls.py
+++ okwarning/okwarning/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from okwarning.views import *
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^new/', NewWarningTemplateView.as_view(), name='new'),
 ]
---
+++ okwarning/okwarning/views/new.py
@@ -0,0 +1,5 @@
+from django.views.generic import TemplateView
+
+
+class NewWarningTemplateView(TemplateView):
+    template_name = 'new.html'
