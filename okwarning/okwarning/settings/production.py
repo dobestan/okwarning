f439be4c85434077eb237ebcd1a518bfcc2576d2
Suchan An <dobestan@gmail.com>
2015-12-19 21:25:19 +0900

    update static settings for production

--- okwarning/okwarning/settings/production.py
+++ okwarning/okwarning/settings/production.py
@@ -8,5 +8,16 @@ ALLOWED_HOSTS = [
 ]
 
 
-STATIC_URL = "http://static.okwarning.or.kr/"
-MEDIA_URL = "http://media.okwarning.or.kr/"
+# Static/Media File Management
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'okwarning.storage.S3PipelineManifestStorage'
+
+STATIC_URL = 'https://derlz5b7zq0xp.cloudfront.net/'
+
+
+# AWS Credentials
+
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
