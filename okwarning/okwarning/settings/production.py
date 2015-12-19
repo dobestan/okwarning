15c2411263150a0c7682ecb99e9672c7fb350654
Suchan An <dobestan@gmail.com>
2015-12-19 22:44:07 +0900

    update production static settings : will use nginx for managing static files

--- okwarning/okwarning/settings/production.py
+++ okwarning/okwarning/settings/production.py
@@ -8,16 +8,4 @@ ALLOWED_HOSTS = [
 ]
 
 
-# Static/Media File Management
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'okwarning.storage.S3PipelineManifestStorage'
-
-STATIC_URL = 'https://derlz5b7zq0xp.cloudfront.net/'
-
-
-# AWS Credentials
-
-AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+STATIC_URL = 'https://awesome.okwarning.or.kr/'
