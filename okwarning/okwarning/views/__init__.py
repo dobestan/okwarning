9767e83b5f034b7684b4b9d37f6c443bea5d91ac
Suchan An <dobestan@gmail.com>
2015-12-19 23:46:23 +0900

    add facebook related views

--- okwarning/okwarning/views/__init__.py
+++ okwarning/okwarning/views/__init__.py
@@ -1,2 +1,3 @@
 from .home import *
 from .new import *
+from .facebook import *
--- okwarning/okwarning/views/home.py
+++ okwarning/okwarning/views/home.py
@@ -6,3 +6,5 @@ from proxy.models import Provider
 class HomeView(ListView):
     model = Provider
     template_name = 'home.html'
+
+    context_object_name = 'providers'
