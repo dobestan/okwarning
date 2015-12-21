0638eb37c9b0f1a774f7dc49381b4cdc0c6b17d3
Suchan An <dobestan@gmail.com>
2015-12-21 22:46:17 +0900

    update HistoryListCreateAPIView : enable search functionality

--- okwarning/proxy/api/history.py
+++ okwarning/proxy/api/history.py
@@ -1,11 +1,12 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework import filters
 
 from proxy.models import History
 from proxy.serializers import HistoryModelSerializer
 
 
 class HistoryListCreateAPIView(ListCreateAPIView):
+    queryset = History.objects.all()
     serializer_class = HistoryModelSerializer
-
-    def get_queryset(self):
-        return History.objects.order_by().distinct('url', )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('url', )
